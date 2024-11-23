bab8185214ab627d80d39a75c4a2f0de78a631cd
Mike Haiduk <x29a100@gmail.com>
2024-11-23 18:41:18 +0100

    fix slider

--- home/controller.js
+++ home/controller.js
@@ -14,15 +14,28 @@ function addHome($scope) {
         openTokenProfile(domain)
     }
 
+
     function init() {
-        postContract("mfm-wallet", "home/api/main.php", {
-        }, function (response) {
+        postContract("mfm-wallet", "home/api/main.php", {}, function (response) {
             $scope.top_mining = response.top_mining
             $scope.top_exchange = response.top_exchange
             $scope.slides = ["oak_log", "rock", "usdt", "usdt2", "usd3t2"]
             $scope.showBody = true
+            //startAnimation()
             $scope.$apply()
         })
+    }
+
+    $scope.slideIndex = 0
+    let interval = null
+
+    function startAnimation() {
+        if (interval != null)
+            clearInterval(interval)
+        interval = setInterval(function () {
+            $scope.slideIndex = ($scope.slideIndex + 1) % $scope.slides.length
+            $scope.$apply()
+        }, 3000)
     }
 
     init()
