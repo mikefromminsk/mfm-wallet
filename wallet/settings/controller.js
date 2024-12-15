ccf1f8360cbbc2ce53e01e052ae4d211d0876fc2
Mike Haiduk <x29a100@gmail.com>
2024-12-15 04:11:21 +0100

    delegated

--- wallet/settings/controller.js
+++ wallet/settings/controller.js
@@ -1,14 +1,24 @@
 function openSettings(success) {
     showDialog('/mfm-wallet/wallet/settings/index.html?nocache', success, function ($scope) {
+        $scope.language = getLanguage()
 
-            $scope.language = getLanguage()
-
-            $scope.logout = function () {
-                openAskSure(function () {
-                    wallet.logout()
-                    storage.setString(storageKeys.onboardingShowed, "true")
-                })
-            }
+        $scope.logout = function () {
+            openAskSure(function () {
+                wallet.logout()
+                storage.setString(storageKeys.onboardingShowed, "true")
+            })
         }
-    )
+
+        $scope.init = function () {
+            postContract("mfm-token", "account.php", {
+                domain: wallet.gas_domain,
+                address: wallet.address(),
+            }, function (response) {
+                $scope.account = response
+                $scope.$apply()
+            })
+        }
+
+        $scope.init()
+    })
 }
