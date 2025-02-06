15863187822bd96882366f5fa184de70ae49d54e
Mike Haiduk <x29a100@gmail.com>
2025-02-06 14:57:06 +0100

    finish bugfixing

--- earn/mining/controller.js
+++ earn/mining/controller.js
@@ -52,7 +52,7 @@ function addMining($scope, domain) {
         worker.addEventListener('message', function (e) {
             $scope.speed = e.data.speed
             if ($scope.last_hash == e.data.last_hash) {
-                postContractWithGas("mfm-mining", "mint10", {
+                postContractWithGas("mfm-mining", "mint20", {
                     domain: domain,
                     nonce: e.data.nonce,
                     str: e.data.str,
