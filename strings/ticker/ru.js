c5775e874b013e67bdaa3e88940e00c49af006d3
Mike Haiduk <x29a100@gmail.com>
2025-09-05 15:15:35 +0300

    gpt translator

--- strings/ticker/ru.js
+++ strings/ticker/ru.js
@@ -1,6 +1,6 @@
 window.ticker = window.ticker || {}
 
 ticker.vavilon = "Вавилон"
-ticker["7dicks_soup"] = "7-диковый суп"
+ticker["7dicks_soup"] = "Суп из 7 диков"
 ticker.dick = "Дик"
 ticker.water = "Вода"
