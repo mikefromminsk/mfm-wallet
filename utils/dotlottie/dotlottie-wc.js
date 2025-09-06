f68a3902a5b121aa90565330938e66c331f1eafb
Mike Haiduk <x29a100@gmail.com>
2025-09-06 18:27:27 +0300

    dotlottie local

---
+++ utils/dotlottie/dotlottie-wc.js
@@ -0,0 +1,2 @@
+export{a as DotLottieWC}from'./chunk-7HSK6UHD.js';import'./chunk-JQHSOFFU.js';import'./chunk-SI7D3PZW.js';//# sourceMappingURL=dotlottie-wc.js.map
+//# sourceMappingURL=dotlottie-wc.js.map
