c55e5f321158127411a4e4cef71ca4548fdcd5a6
Mike Haiduk <x29a100@gmail.com>
2025-09-15 23:38:03 +0300

    add chart

--- profile/chart/controller.js
+++ profile/chart/controller.js
@@ -86,10 +86,11 @@ function addChart($scope, app, key, accumulate_key) {
     }
 
     $scope.periods = ['M', 'H', 'D']
-    $scope.period_name = storage.getString(storageKeys.chart_period, 'H')
+    $scope.period_name = window.storage ? window.storage.getString(storageKeys.chart_period, 'H') : "H"
     $scope.setPeriod = function (period_name) {
         $scope.period_name = period_name
-        storage.setString(storageKeys.chart_period, period_name)
+        if (window.storage)
+            window.storage.setString(storageKeys.chart_period, period_name)
         postContract("mfm-chart", "chart", {
             app: app,
             key: key,
