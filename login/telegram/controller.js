316a7cddac83dfca067edd36c44e4bee0e4e1102
Mike Haiduk <x29a100@gmail.com>
2025-10-03 16:09:14 +0300

    bugfix

--- login/telegram/controller.js
+++ login/telegram/controller.js
@@ -1,27 +1,28 @@
 function openTelegramLogin(bot_name, success) {
     if (wallet.address() == "" && getTelegramUserId() != null) {
         showDialog("login/telegram", success, function ($scope) {
-            postContract("mfm-telegram", "subscription_check", {
-                user_id: getTelegramUserId()
-            }, function () {
-                postContract("mfm-telegram", "login", {
+            $scope.login = function () {
+                postContract("mfm-telegram", "subscription_check", {
                     user_id: getTelegramUserId()
-                }, function (response) {
-                    let password = hash(response.seed)
-                    let address = hashAddress(password)
-                    wallet.login(address, password, function () {
-                        $scope.close()
-                        $scope.openOnboarding()
-                    }, function (message) {
-                        if (message == "invalid password")
-                            showError(str.password_invalid)
+                }, function () {
+                    postContract("mfm-telegram", "login", {
+                        user_id: getTelegramUserId()
+                    }, function (response) {
+                        let password = hash(response.seed)
+                        let address = hashAddress(password)
+                        wallet.login(address, password, function () {
+                            $scope.close()
+                            $scope.openOnboarding()
+                        }, function (message) {
+                            if (message == "invalid password")
+                                showError(str.password_invalid)
+                        })
                     })
                 })
-            }, function () {
-                $scope.close = function () {
-                    Telegram.WebApp.close()
-                }
-            })
+            }
+
+            $scope.login()
+
         })
     }
 }
